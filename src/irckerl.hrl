7219f98fb7b4cebd91c527f251b8acefc202f83e
ckruse <cjk@wwwtech.de>
2011-08-21 00:21:19 +0200

    added header file for DEBUG etc macros

---
+++ src/irckerl.hrl
@@ -0,0 +1,27 @@
+%% Copyright (C) 2011 by Christian Kruse <cjk@wwwtech.de>
+
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+
+-define(DEFAULT_MAX_CLIENTS, 2048).
+-define(TIMEOUT, 180000).
+
+-define(DEBUG(X),error_logger:info_msg(X)).
+-define(DEBUG(X,Y),error_logger:info_msg(X,Y)).
+
+% eof
