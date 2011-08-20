8727afe86bd042f4be68e003a5470f6557d69676
ckruse <cjk@wwwtech.de>
2011-08-21 00:20:56 +0200

    added prefix parser

--- src/irckerl_parser.erl
+++ src/irckerl_parser.erl
@@ -24,7 +24,7 @@
 
 -compile([verbose, report_errors, report_warnings, trace, debug_info]).
 
--export([parse_client/1, normalize_nick/1, valid_nick/1]).
+-export([parse/1, normalize_nick/1, valid_nick/1]).
 
 
 normalize_nick("[" ++ Rest)  -> "{" ++ normalize_nick(Rest);
@@ -40,9 +40,54 @@ valid_nick(Nick) ->
         _Other -> invalid
     end.
 
+parse_prefix(<<":",Prefix/binary>>) ->
+    parse_prefix(Prefix);
+parse_prefix(<<Prefix/binary>>) ->
+    io:format("prefix: ~p~n",[Prefix]),
+    case re:run(Prefix,"^([a-z0-9.-]+)$",[{capture,none}]) of
+        match ->
+            {ok, [binary_to_list(Prefix)]};
+
+        nomatch ->
+            case re:run(Prefix,"^([a-zA-Z][a-zA-Z0-9\\[\\]\\\\^{}`-]+)(?:!([^\s@]+)(?:@([a-z0-9.-]+))?)?$",[{capture,all_but_first}]) of
+                {match,[{StartN, EndN}]} ->
+                    {ok, [string:substr(binary_to_list(Prefix),StartN+1,EndN),"",""]};
+
+                {match,[{StartN, EndN},{StartU,EndU}]} ->
+                    LPrefix = binary_to_list(Prefix),
+                    {ok,[string:substr(LPrefix,StartN+1,EndN),string:substr(LPrefix,StartU+1,EndU),""]};
+
+                {match,[{StartN, EndN},{StartU,EndU},{StartH,EndH}]} ->
+                    LPrefix = binary_to_list(Prefix),
+                    {ok,[string:substr(LPrefix,StartN+1,EndN),string:substr(LPrefix,StartU+1,EndU),string:substr(LPrefix,StartH+1,EndH)]};
+
+                nomatch ->
+                    {error, "Prefix parse: not matched to prefix regex"};
+                _Other ->
+                    {error, "Prefix parse: not matched to prefix regex"}
+            end;
+
+        _Other ->
+            {error, "Prefix parse: not matched to prefix regex"}
+    end.
 
 
-parse_client(<<Message/binary>>) ->
+parse(<<":",Message/binary>>) -> % a message with a prefix
+    [PrefixS,LastS] = re:split(Message,"\s+",[{parts,2}]),
+    case parse_prefix(PrefixS) of
+        {ok, Prefix} ->
+            case parse(LastS) of
+                {ok, Cmd, Params} ->
+                    {ok, Prefix, Cmd, Params};
+                {error, Reason} ->
+                    {error, Reason}
+            end;
+
+        {error, Reason} ->
+            {error, Reason}
+    end;
+
+parse(<<Message/binary>>) -> % a message w/o prefix
     case re:run(Message,"^([a-zA-Z]+|\\d\\d\\d)",[{capture,all_but_first}]) of
         {match,[{Start,Len}]} ->
             LMessage = binary_to_list(Message),
