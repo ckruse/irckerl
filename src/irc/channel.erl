bc6430f195c461e638d5516dc507c2aed3ff66e6
Christian Kruse <cjk@wwwtech.de>
2011-12-02 22:13:50 +0100

    bugfix: use new #channel.members data structure

--- src/irc/channel.erl
+++ src/irc/channel.erl
@@ -33,14 +33,15 @@
 
 -spec join(#channel_state{}, #channel{}, #user{}) -> {reply, {ok, [string()]}, #channel_state{}}.
 join(State, Chan, User = #user{nick = Nick, username = Username, host = Host}) ->
-    Clients = Chan#channel.members ++ [User],
+    Clients = Chan#channel.members ++ [{Nick, User}],
 
-    Names = lists:map(fun(#user{nick = N, pid = CPid}) ->
-        gen_fsm:send_event(CPid, {join, Nick ++ "!" ++ Username ++ "@" ++ Host, Chan#channel.name}),
-        N
-    end, Clients),
+    Names = lists:map(fun({N, #user{pid = CPid}}) ->
+                              gen_fsm:send_event(CPid, {join, Nick ++ "!" ++ Username ++ "@" ++ Host, Chan#channel.name}),
+                              N
+                      end, Clients),
 
-    {reply, {ok, Names}, State#channel_state{channel=Chan#channel{members=Clients}}}.
+    TheChan = Chan#channel{members = Clients},
+    {reply, {ok, Names}, State#channel_state{channel = TheChan}}.
 
 -spec part(#channel_state{}, #channel{}, #user{}, string()) -> {reply, ok, #channel_state{}}.
 part(State, Chan, User, Reason) ->
